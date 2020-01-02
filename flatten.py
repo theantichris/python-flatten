9d44f788b1cfd97c4bdd511e284508cc2f3691f3
Christopher Lamm <chris@theantichris.com>
2020-01-02 08:45:23 -0500

    flattening a list

---
+++ flatten.py
@@ -0,0 +1,15 @@
+def flatten(my_list):
+    result = []
+
+    for i in my_list:
+        if isinstance(i, list):
+            flat_list = flatten(i)
+            result += flat_list
+        else:
+            result.append(i)
+
+    return result
+
+
+planets = ['mercury', 'venus', ['earth'], 'mars', [['jupiter', 'saturn']], 'uranus', ['neptune', 'pluto']]
+print(flatten(planets))
